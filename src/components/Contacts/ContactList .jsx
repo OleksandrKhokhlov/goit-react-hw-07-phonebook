318c41e6957e1037ab4c9a4c1b7cb5461b1fa2bf
OleksandrKhokhlov <hohlovsanya@gmail.com>
2023-09-25 16:52:37 +0300

    bag fix

--- src/components/Contacts/ContactList .jsx
+++ src/components/Contacts/ContactList .jsx
@@ -5,32 +5,38 @@ import {
   ContactsItem,
   ContactsList,
 } from './ContactList .styled';
-import { getContacts, getFilter} from 'redux/selectors';
+import { getContacts, getError, getFilter } from 'redux/selectors';
 import { deleteContact } from 'redux/operations';
+import toast from 'react-hot-toast';
 
 export const Contacts = () => {
   const dispatch = useDispatch();
   const filter = useSelector(getFilter);
   const contacts = useSelector(getContacts);
+  const errorMessage = useSelector(getError);
 
   return (
     <>
       <Filter />
-      <ContactsList>
-        {contacts
-          .filter(({ name }) => name.toLowerCase()?.includes(filter))
-          .map(({ id, name, number }) => (
-            <ContactsItem key={id}>
-              {name}: {number}
-              <ButtonDelete
-                type="button"
-                onClick={() => dispatch(deleteContact(id))}
-              >
-                Delete
-              </ButtonDelete>
-            </ContactsItem>
-          ))}
-      </ContactsList>
+      {errorMessage ? ( 
+        toast.error(errorMessage)
+      ) : ( 
+        <ContactsList>
+          {contacts
+            .filter(({ name }) => name.toLowerCase()?.includes(filter))
+            .map(({ id, name, number }) => (
+              <ContactsItem key={id}>
+                {name}: {number}
+                <ButtonDelete
+                  type="button"
+                  onClick={() => dispatch(deleteContact(id))}
+                >
+                  Delete
+                </ButtonDelete>
+              </ContactsItem>
+            ))}
+        </ContactsList>
+      )}
     </>
   );
 };
